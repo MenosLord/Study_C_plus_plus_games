842817e54f9c714d1fbaa7c759e940053fa01a04
MenosLord <lordmalsagov@gmail.com>
2023-03-23 19:53:02 +0300

    add inventory_2.cpp

---
+++ hangman/inventory_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string>inventory;
+    inventory.push_back("sword");
+    inventory.push_back("armor");
+    inventory.push_back("shield");
+    cout << inventory[0] << endl;
+    return 0;
+
+
+}
