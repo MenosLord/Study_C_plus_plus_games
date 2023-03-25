03bda3af1f46d23ca4dd18778235f840e723e8ee
MenosLord <lordmalsagov@gmail.com>
2023-03-25 12:21:45 +0300

    update high_scores.cpp

--- hangman/high_scores.cpp
+++ hangman/high_scores.cpp
@@ -17,7 +17,32 @@ int main() {
     for (iter = scores.begin(); iter!=scores.end(); ++iter){
         cout << *iter << endl;
     }
+    cout << "\nFinding scores.";
+    int score;
+    cout << "\nEnter a score to find: ";
+    cin >> score;
+    iter=find(scores.begin(), scores.end(), score);
+    if (iter!=scores.end()) {
+        cout << "Score found.\n";
+    } else {
+        cout << "Score not found.\n";
+    }
+    cout << "\nRandomazing scores.";
+    srand(static_cast<unsigned int>(time(0)));
+    random_shuffle(scores.begin(), scores.end());
+    cout << "\nHigh scores:\n";
+    for (iter = scores.begin(); iter!=scores.end(); ++iter){
+        cout << *iter << endl;
+    }
+    cout << "\nSorting scores.";
+    sort(scores.begin(), scores.end());
+    cout << "\nHigh scores:\n";
+    for (iter = scores.begin(); iter!=scores.end(); ++iter){
+        cout << *iter << endl;
+    }
     return 0;
+
+
 
 
 }
