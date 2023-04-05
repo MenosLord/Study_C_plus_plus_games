23f776bab3d7e3ee62b55b5379340ac4cb987c8a
MenosLord <lordmalsagov@gmail.com>
2023-04-05 18:49:12 +0300

    update x_o.cpp

--- noliki/x_o.cpp
+++ noliki/x_o.cpp
@@ -14,3 +14,62 @@
     Передать ход сопернику
     Конец игры. Поздравить победителя или объявить ничью
 */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// прототипы функций
+void instructions(); // отображает правила игры
+char askYesorNot(string question); // задает вопрос ожидая ответ "Да" или "Нет"
+int askNumbers(string question, int high, int low =0); // запрашивает число из диапазонаб получает вопрос, малое число или большее
+                                                      // возвращает число в диапозоне от low до high
+char humanPiece(); //определяет какими фигурами будет ходить пользователь. Возвращает X или O
+char opponent(char piece); // ответ компа на ход человека
+void displayBoard(const vector<char>& board); // отрисовка поля, получает поле
+char winner(const vector<char>& board); // определяет победителя игры. Возвращает либо Х, либо О, либо T(ничья), либо N(еще не определено)
+bool isLegal(const vector<char>&board, int move); // проверка на корректность хода
+int humanMove(const vector<char>&board, char human); // узнает ход пользователя
+int computerMove(vector<char>&board, char computer); // узнает ход компа
+void announceWinner(char winner, char computer, char human); // оглашение результа. Победа компа, либо человека, либо ничья
+
+
+// глобальные константы
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T'; // ничья
+const char NO_ONE = 'N'; // продолжение игры
+
+
+int main() {
+    int move;
+    const int NUM_SQUARES = 9;
+    vector<char> board(NUM_SQUARES, EMPTY);
+    instructions();
+    char human = humanPiece();
+    char computer = opponent(human);
+    char turn = X;
+    displayBoard(board);
+    while (winner(board) == NO_ONE) {
+        if (turn == human){
+            move = humanMove(board, human);
+            board[move] = human;
+        } else {
+            move = computerMove(board, computer);
+            board[move] = computer;
+        }
+        displayBoard(board);
+        turn = opponent(turn);
+    }
+    announceWinner(winnner(board), computer, human);
+    return 0;
+}
+
+
+
+
+
+
