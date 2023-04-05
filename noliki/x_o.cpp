a93c00c5a7e8ac4d8dbf3cafc580e0bda4480874
MenosLord <lordmalsagov@gmail.com>
2023-04-05 19:38:31 +0300

    update x_o.cpp

--- noliki/x_o.cpp
+++ noliki/x_o.cpp
@@ -121,3 +121,45 @@ char opponent(char piece){
     }
 }
 
+void displayBoard(const vector<char>& board){
+    cout << "\n\t" << board[0] << " | " << board[1] << " | " << board[2];
+    cout << "\n\t" << "---------";
+    cout << "\n\t" << board[3] << " | " << board[4] << " | " << board[5];
+    cout << "\n\t" << "---------";
+    cout << "\n\t" << board[6] << " | " << board[7] << " | " << board[8];
+    cout << "\n\n";
+}
+
+char winner(const vector<char>& board){
+    // перечисляем выигрышные комбинации
+    const int WINNING_ROWS[8][3] = { {0, 1, 2},
+                                     {3, 4, 5},
+                                     {6, 7, 8},
+                                     {0, 3, 6},
+                                     {1, 4, 7},
+                                     {2, 5, 8},
+                                     {0, 4, 8},
+                                     {2, 4, 6} };
+    const int TOTAL_ROWS = 8;
+    // если в одном из выигрышных рядов уже есть три одинак-х значения (и они != EMPTY), то победитель определился
+    for(int row = 0; row < TOTAL_ROWS; ++row)
+    {
+        if ( (board[WINNING_ROWS[row][0]] != EMPTY) &&
+             (board[WINNING_ROWS[row][0]] == board[WINNING_ROWS[row][1]]) &&
+             (board[WINNING_ROWS[row][1]] == board[WINNING_ROWS[row][2]]) )
+        {
+            return board[WINNING_ROWS[row][0]];
+        }
+    }
+
+    // так как победителя еще нет, проверяем на ничью
+    if (count(board.begin(), board.end(), EMPTY) == 0) {
+        return TIE;
+    }
+
+    return NO_ONE;
+}
+
+bool isLegal(const vector<char>&board, int move){
+    return (board[move] == EMPTY);
+}
