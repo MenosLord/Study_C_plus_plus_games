302fdaa771f1bc26779792ac2b1423e35192a797
MenosLord <lordmalsagov@gmail.com>
2023-04-07 19:58:07 +0300

    update pointing.cpp

--- noliki_2/pointing.cpp
+++ noliki_2/pointing.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int main() {
-    int* pAPointer; //объявялем указатель
     int* pScore = 0; // объявляем и инициализируем указатель
     int score = 1000;
     pScore = &score; //присваиваем указателю адрес переменной score
@@ -13,7 +12,27 @@ int main() {
     cout << "pScore is: " << pScore << "\n"; // адрес сохраненный в указателе
     cout << "score is: " << score << "\n";
     cout << "*pScore is: " << *pScore << "\n"; // значение на которое направлен указатель
-    
-
-
+    cout << "Adding 500 to score\n";
+    score += 500;
+    cout << "score is: " << score << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+    cout << "Adding 500 to *pScore\n";
+    *pScore += 500;
+    cout << "score is: " << score << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+    cout << "Assigning &newScore to pScore\n";
+    int newScore = 5000;
+    pScore = &newScore;
+    cout << "&newScore is:" << &newScore << "\n";
+    cout << "pScore is: " << pScore << "\n";
+    cout << "newScore is: " << newScore << "\n";
+    cout << "*pScore is: " << *pScore << "\n\n";
+    cout << "Assigning &str to pStr\n";
+    string str = "score";
+    string* pStr = &str; // указатель на строку
+    cout << "Str is: " << str << "\n";
+    cout << "*pSrt is: " << *pStr << "\n";
+    cout << "(*pStr).size() is: " << (*pStr).size() << "\n";
+    cout << "pStr->size() is: " << pStr->size() << "\n";
+    return 0;
 }
